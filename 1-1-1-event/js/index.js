21652da3f8d3f578e8e22477e6094055406e881c
Johnny <johnnybrot@gmail.com>
2023-01-11 20:31:00 +0800

    本次新Jquery

---
+++ 1-1-1-event/js/index.js
@@ -0,0 +1,10 @@
+$("h1").on("click", getTitle);
+$("p").on("click", getParagraph);
+
+function getTitle() {
+    alert("我點到標題一");
+}
+
+function getParagraph() {
+    alert('我點到段落')
+}
---
+++ 1-1-2-event/js/index.js
@@ -0,0 +1,11 @@
+$("h1").click(function() {
+    alert("我點到標題一");
+}
+)
+
+$("p").click(function() {
+    alert('我點到段落');
+}
+)
+
+
